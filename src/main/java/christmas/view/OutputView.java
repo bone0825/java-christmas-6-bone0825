251a4037099909a42179f30ed7b1f72e9dc9455d
bone0825 <quddlf775@naver.com>
2023-11-11 14:41:07 +0900

    feat: inputMessage.java, OutputMessage.java update

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,4 +1,13 @@
 package christmas.view;
 
+import java.text.DecimalFormat;
+
 public class OutputView {
+    private static final String OUTPUT_PRICE_FORMAT = "#,###.#";
+
+    private static String outputPriceFormat (int price){
+        DecimalFormat format = new DecimalFormat(OUTPUT_PRICE_FORMAT);
+        return format.format(price);
+    }
+
 }
